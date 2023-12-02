cefa90097666851c22094705f342a60ed9f202e1
Daniil Kuznetsov <daniilbacardi@yandex.ru>
2023-12-02 18:48:56 +0300

    initial commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,19 +2,42 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attachments;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
+    @Tag("all-tests")
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @AfterEach
+    @Tag("all-tests")
     void afterEach() {
+        Attachments.screenshotAs("LastScreenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
         Selenide.closeWebDriver();
     }
 }
